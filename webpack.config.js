2b612a265b075768a99d0f8ad26b6ec7b0334f5a
Ham I-sik(a.k.a 2sik, bis) <ham2sik@gmail.com>
2019-10-16 16:42:36 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+const cssExports = require("./css-config"); // css config object
+const jsExports = require("./js-config"); // js config object
+
+// Return Array of Configurations
+module.exports = [cssExports.common, cssExports.guide, cssExports.page_template, jsExports.event.summerFestival_2019, jsExports.test];
