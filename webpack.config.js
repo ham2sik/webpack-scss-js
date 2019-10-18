5eb44d7b56aaf1a5f8eda2b6d46861f506039639
Ham I-sik(a.k.a 2sik, bis) <ham2sik@gmail.com>
2019-10-18 09:52:37 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 const cssExports = require("./css-config"); // css config object
 const jsExports = require("./js-config"); // js config object
+const imgExports = require("./img-config"); // img config object
 
 // Return Array of Configurations
 module.exports = [cssExports.common, cssExports.guide, cssExports.page_template, jsExports.event.summerFestival_2019, jsExports.test];
+// module.exports = [imgExports.common];
